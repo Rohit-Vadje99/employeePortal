b3b9a75a63761b0185b183fc4f68a45669243841
Rohit Vadje-LocalLaptop <rohitvadje1999@gmai.com>
2026-03-15 18:26:14 +0530

    add catchError

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,22 +10,47 @@ export class EmployeeService {
   constructor() {}
 
   getAllEmployees() {
-    return this.http.get('https://dummyjson.com/users?limit=0');
+    return this.http.get('https://dummyjson.com/users?limit=0').pipe(
+      catchError((err) => {
+        console.error(err);
+        return throwError(() => err);
+      }),
+    );
   }
 
   getEmployee(id: string) {
-    return this.http.get('https://dummyjson.com/users/' + id);
+    return this.http.get('https://dummyjson.com/users/' + id).pipe(
+      catchError((err) => {
+        console.error(err);
+        return throwError(() => err);
+      }),
+    );
   }
 
   addEmployee(employee: any) {
-    return this.http.post('https://dummyjson.com/users/add', employee);
+    return this.http.post('https://dummyjson.com/users/add', employee).pipe(
+      catchError((err) => {
+        console.error(err);
+        return throwError(() => err);
+      }),
+    );
   }
 
   editEmployee(id: string, employee: any) {
-    return this.http.patch('https://dummyjson.com/users/' + id, employee);
+    return this.http.patch('https://dummyjson.com/users/' + id, employee).pipe(
+      catchError((err) => {
+        console.error(err);
+        return throwError(() => err);
+      }),
+    );
   }
 
   searchEmployee(name: any) {
-    return this.http.get('https://dummyjson.com/users/search?q=' + name);
+    return this.http.get('https://dummyjson.com/users/search?q=' + name).pipe(
+      catchError((err) => {
+        console.error(err);
+        return throwError(() => err);
+      }),
+    );
   }
 }
